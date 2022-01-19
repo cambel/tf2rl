b737ccce199a3963afcccec62babdccf7659ba5d
Cristian Beltran <cristianbehe@gmail.com>
2022-01-19 11:55:22 +0900

    up to tf 2.7

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ except ImportError:
     install_requires.append(f"tensorflow=={tf_version}")
     pass
 
+compatible_tfp = {"2.7": ["tensorflow-probability~=0.15.0"],
 compatible_tfp = {"2.4": ["tensorflow-probability~=0.12.0"],
                   "2.3": ["tensorflow-probability~=0.11.0"],
                   "2.2": ["tensorflow-probability~=0.10.0"],
