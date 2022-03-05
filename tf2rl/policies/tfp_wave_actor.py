17b3d2e252c4a401c809d7bda491475a6360612d
Cristian Beltran <cristianbehe@gmail.com>
2022-03-05 13:58:09 +0900

    Update max level of randomness during training
    was crazy high and kind of annoying to handle force control, maybe high randomness is useful for position control only

--- tf2rl/policies/tfp_wave_actor.py
+++ tf2rl/policies/tfp_wave_actor.py
@@ -7,7 +7,7 @@ from tf2rl.policies.tcn import compiled_tcn
 layers = tf.keras.layers
 
 class WaveFTActor(tf.keras.Model):
-    LOG_STD_CAP_MAX = 2  # np.e**2 = 7.389
+    LOG_STD_CAP_MAX = -2  # np.e**-1 = 0.3679 || np.e**2 = 7.389
     LOG_STD_CAP_MIN = -20  # np.e**-10 = 4.540e-05
     EPS = 1e-6
 
