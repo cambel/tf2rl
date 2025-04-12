aad2a8b0299d2e21f945615c0bc51a577caf54d6
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2025-04-12 14:14:34 +0900

    Ignore atari dependencies

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ install_requires = [
     "future",
     "scipy",
     "scikit-image",
-    "gym[atari]==0.17.0"]
+    # "gym[atari]==0.17.0"
+    ]
 
 tf_version = "2.12"  # Default Version
 try:
@@ -40,17 +41,17 @@ if not (platform.system() == 'Windows' and tf_version == "2.0"):
     install_requires.append(*compatible_tfa[tf_version])
 
 
-compatible_gym = {
-    "2.0": "gym[atari]<0.21.0",
-    "2.1": "gym[atari]<0.21.0"
-}
-gym_version = compatible_gym.get(tf_version, "gym[atari]>=0.21.0")
+# compatible_gym = {
+#     "2.0": "gym[atari]<0.21.0",
+#     "2.1": "gym[atari]<0.21.0"
+# }
+# gym_version = compatible_gym.get(tf_version, "gym[atari]>=0.21.0")
 
 extras_require = {
     "tf": ["tensorflow>=2.0.0"],
     "tf_gpu": ["tensorflow-gpu>=2.0.0"],
-    "examples": [gym_version, "opencv-python"],
-    "test": ["coveralls", gym_version, "matplotlib", "opencv-python", "future"]
+    # "examples": [gym_version, "opencv-python"],
+    "test": ["coveralls", "opencv-python", "future"]
 }
 
 setup(
